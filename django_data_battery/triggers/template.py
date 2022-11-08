161b94b51537b4c3a5bf79cbea5aed8c03683ca9
Vanya U <ivict@rambler.ru>
2022-11-09 02:05:16 +0300

    # Thee are templates for the mysql and postgresql.
    Signed-off-by: Vanya U <ivict@rambler.ru>

--- django_data_battery/triggers/template.py
+++ django_data_battery/triggers/template.py
@@ -13,17 +13,55 @@ END;
 
 '''
 
+INSERT_TRIGGER_TEMPLATE_POSTGRES='''
+
+CREATE OR REPLACE FUNCTION process_emp_audit() RETURNS TRIGGER AS $emp_audit$
+    BEGIN
+        --
+        -- Create rows in emp_audit to reflect the operations performed on emp,
+        -- making use of the special variable TG_OP to work out the operation.
+        --
+        IF (TG_OP = 'DELETE') THEN
+            INSERT INTO emp_audit
+                SELECT 'D', now(), user, o.* FROM old_table o;
+        ELSIF (TG_OP = 'UPDATE') THEN
+            INSERT INTO emp_audit
+                SELECT 'U', now(), user, n.* FROM new_table n;
+        ELSIF (TG_OP = 'INSERT') THEN
+            INSERT INTO emp_audit
+                SELECT 'I', now(), user, n.* FROM new_table n;
+        END IF;
+        RETURN NULL; -- result is ignored since this is an AFTER trigger
+    END;
+$emp_audit$ LANGUAGE plpgsql;
+
+CREATE TRIGGER emp_audit_ins
+    AFTER INSERT ON emp
+    REFERENCING NEW TABLE AS new_table
+    FOR EACH STATEMENT EXECUTE FUNCTION process_emp_audit();
+
+
+'''
+
+
+INSERT_TRIGGER_TEMPLATE_MYSQL='''
+
+CREATE TRIGGER upd_check BEFORE UPDATE ON account
+       FOR EACH ROW
+       BEGIN
+           IF NEW.amount < 0 THEN
+               SET NEW.amount = 0;
+           ELSEIF NEW.amount > 100 THEN
+               SET NEW.amount = 100;
+           END IF;
+       END;//
+
+
+'''
+
 UPDATE_TRIGGER_TEMPLATE='''
 
-CREATE TRIGGER validate_email_before_insert_leads 
-   BEFORE INSERT ON leads
-BEGIN
-   SELECT
-      CASE
-	WHEN NEW.email NOT LIKE '%_@__%.__%' THEN
-   	  RAISE (ABORT,'Invalid email address')
-       END;
-END;
+
 '''
 
 DELETE_TRIGGER_TEMPLATE='''
---
+++ django_data_battery/utils/triggers_factory.py
@@ -0,0 +1,15 @@
+
+class TriggersFactory:
+    
+    @classmethod
+    def create_trigger_on_delete(cls, table_name):
+        pass
+
+    @classmethod
+    def create_trigger_on_insert(cls, table_name):
+        pass
+
+    @classmethod
+    def create_trigger_on_update(cls, table_name):
+        pass
+    
