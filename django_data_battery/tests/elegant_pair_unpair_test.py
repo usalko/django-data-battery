408a1550b02cb9e4fc93549e404ea791eb3f5af1
Vanya U <ivict@rambler.ru>
2022-11-08 13:38:25 +0300

    # There is the fix for szudzik functions

--- django_data_battery/tests/elegant_pair_unpair_test.py
+++ django_data_battery/tests/elegant_pair_unpair_test.py
@@ -1,13 +1,15 @@
 from unittest import TestCase
 from math import sqrt, floor
 
+
 def _elegant_pair(x, y):
     '''
     function elegantPair(x, y) {
         return (x >= y) ? (x * x + x + y) : (y * y + x);
     }
     '''
-    return y * y + x if x < y else x * x +x + y
+    return x * x + x + y if x >= y else y * y + x
+
 
 def _elegant_unpair(z):
     '''
@@ -17,14 +19,18 @@ def _elegant_unpair(z):
         return ((z - sqz) >= sqrtz) ? [sqrtz, z - sqz - sqrtz] : [z - sqz, sqrtz];
     }
     '''
-    sqrtz = floor(sqrt(z))
+    sqrtz = int(floor(sqrt(z)))
     sqz = sqrtz * sqrtz
-    return (z - sqz, sqrtz) if (z - sqrtz) < sqrtz else (sqrtz, z - sqz - sqrtz) 
+    if (z - sqz) >= sqrtz:
+        return (sqrtz, z - sqz - sqrtz)
+    else:
+        return (z - sqz, sqrtz)
+
 
 class ElegantPairUnpairTest(TestCase):
-    
+
     def test_pair_default(self):
-        
+
         x = 1
         y = 2
         self.assertEqual(_elegant_pair(x, y), 5)
@@ -32,9 +38,10 @@ class ElegantPairUnpairTest(TestCase):
     def test_pair_unpair_case1(self):
         x = 1
         y = 2
-        
+
         z = _elegant_pair(x, y)
-        
+        print(z)
+
         x_after, y_after = _elegant_unpair(z)
         self.assertEqual(x, x_after)
         self.assertEqual(y, y_after)
@@ -42,9 +49,9 @@ class ElegantPairUnpairTest(TestCase):
     def test_pair_unpair_case2(self):
         x = 2
         y = 1
-        
+
         z = _elegant_pair(x, y)
-        
+
         x_after, y_after = _elegant_unpair(z)
         self.assertEqual(x, x_after)
         self.assertEqual(y, y_after)
